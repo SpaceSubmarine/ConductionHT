e8ec4181d612f8a7367a775dd93a0fdf7a7a7289
SpaceSubmarine <marc.montalvez@gmail.com>
2022-10-23 19:43:45 +0200

    primera versión previa a la entrega

--- EX_1_V07.py
+++ EX_1_V07.py
@@ -7,7 +7,6 @@ import matplotlib.pyplot as plt
 
 ## Input-Data ==============================================
 #physical data:
-
 thick = 30 #m
 heigh = 1 #m
 width = 1 #m
@@ -16,30 +15,40 @@ se=sw #m2
 density = 8830 #kg/m3
 Vp=thick*heigh*width #m3
 
-print('Enter Ta of liquid water in Celsius:')
-#T_a = int(input())
-T_a = 25
-print('Enter Tb of liquid water in Celsius:')
-#T_b = int(input())
-T_b = 25
-alpha_a = 2000 #W/(m^2 k)
-alpha_b = 2000 # random number  W/(m^2 k)
+print('Enter temperature of the first fluid T_a in Celsius:')
+T_a = int(input())
+#T_a = 25
+print('Enter temperature of the second fluid T_b in Celsius:')
+T_b = int(input())
+#T_b = 25
+print('Enter the convection heat transfer coefficient of the first fluid "alpha_a":')
+alpha_a = int(input())
+#alpha_a = 2000 #W/(m^2 k)
+print('Enter convection heat transfer coefficient of the second fluid "alpha_b":')
+alpha_b = int(input())
+#alpha_b = 2000 # random number  W/(m^2 k)
+print("Enter the internal heat energy 'q_dot_u' (constant) of the solid material:")
+alpha_b = float(input())
 q_dot = 0.00030 #have problems 
 
 T_a = T_a+273 # Celsius to Kelvin
 T_b = T_b +273 # Celsius to Kelvin
-lambda_1 = 386 #W/mK cooper conduction heat transfer at  20ºC
+
+print('Introduce the conduction heat transfer value (constant), of the chosen material in W/mK :')
+lambda_1 = int(input())
+#lambda_1 = 386 #W/mK cooper conduction heat transfer at  20ºC
 
 # Numerical Data
-print('Enter the number of control volumes N:')
-#N = int(input())
-N = 10
+print('Enter the number of points N:')
+N = int(input())
+# N = 10
 Max_error = (10**(-18))
-print('Enter the solid Temperature in Celsius:')
+print('Enter the temperature of the solid material in Celsius:')
 #T_b = int(input())
+T_input = int(input())
 T_input = 250
 T_input = T_input+273
-Max_iter =30000
+Max_iter =500000
 
 
 #Domain ==============================================
@@ -73,7 +82,6 @@ for i in range(len(T_init)):
 
 
 # Coefficients =============================================
-
 T = T_init
 T_f = np.ones(len(T))
 
@@ -96,35 +104,10 @@ for i in range(len(ap)):
     bp[i] = q_dot * density *Vp
 
 
-
 diff = 100000
 stored_diff = np.ones(Max_iter)
 iteration = 1
 
-
-
-"""
-# OLD Gauss-Seidel
-while diff > Max_error and iteration < Max_iter:    
-    for i in range(1,N+1):        
-        T[0] = T_a#(ae[0]*T[1]+bp[0]) / ap[0]
-        T[-1] = T_b#(aw[-1]*T[-2]+bp[-1]) / ap[-1]
-        T_f[i] =  (aw[i]*T[i-1] + ae[i]*T[i+1] + bp[i])/ap[i]
-        #T_f[i] = (ae[i]*T[i+1] + bp[i]) / ap[i]
-        
-    # vector to plot the error evolution 
-    stored_diff[i] = np.max(T-T_f)
-    
-    diff = np.max(T-T_f)
-
-    T = T_f
-    
-    #print(iteration)
-    print(diff)
-    #print("error:", diff<Max_error)
-    
-    iteration = iteration+1
-"""
 
 #GAUSS-SEIDEL
 while diff > Max_error and iteration < Max_iter:
@@ -136,24 +119,22 @@ while diff > Max_error and iteration < Max_iter:
     iteration = iteration + 1
     stored_diff[i] = np.max(T-T_f)
     diff = float(np.max(abs(T-T_f)))
-    print(diff)
     T=T_f
 
 
-#T[0] = (ae[0]*T[1]+bp[0]) / ap[0]
-#T[-1] = (aw[-1]*T[-2]+bp[-1]) / ap[-1]        
+print("Number of iterations:", iteration)
+if iteration < 50:
+    print("\nThe number of iterations seems to be very lower", 
+    "\n it seems that the code has something wrong with the coefficients \n or with the Gauss-Seidel method")
 
 
-#plt.plot(x_point,T)
-"""
-fig, ax = plt.subplots()
-ax.stackplot(x_point, T)"""
-
+fig1, ax = plt.subplots()
 plt.imshow((x_point,T), interpolation='none', cmap='inferno')
 plt.colorbar()
 plt.show()
 
-#plt.colormaps(x_point,T)
+fig2, ax = plt.subplots()
+plt.stackplot(x_point,T)
 #plt.yscale("log")
-
 plt.show()
+
